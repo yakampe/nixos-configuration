2d696bdae1e97c70856eb9d7933b6aae88a7d707
yakampe <yanis.kampe.cv@gmail.com>
2024-05-01 23:25:15 +0100

    setting the correct home manager path

--- install-home-manager.zsh
+++ install-home-manager.zsh
@@ -7,19 +7,9 @@ nix-channel --update
 nix-shell '<home-manager>' -A install
 
 # Remove the existing Home Manager config file
-rm ~/.config/nixpkgs/home.nix
+rm ~/.config/home-manager/home.nix
 
 # Create a symbolic link to the Home Manager config file
-ln -s $(pwd)/home-manager/home.nix ~/.config/nixpkgs/home.nix
+ln -s $(pwd)/home-manager/home.nix ~/.config/home-manager/home.nix
 
 home-manager switch
-
-# Pick up your Nix config
-# Remove the existing NixOS configuration file
-sudo rm /etc/nixos/configuration.nix
-
-# Create a symbolic link to the NixOS configuration file based on the hostname
-sudo ln -s $(pwd)/nix-config/hosts/$HOST/configuration.nix /etc/nixos/configuration.nix
-
-sudo nixos-rebuild switch
-sudo 
