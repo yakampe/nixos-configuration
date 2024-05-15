0c5518d56fbaec39f00cf786971adb06c3825303
yakampe <yanis.kampe.cv@gmail.com>
2024-05-15 17:40:58 +0100

    syncing config

---
+++ nix-config/hosts/desktop/lightdm.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+
+{
+    services.xserver = {
+        displayManager.lightdm.enable = true;
+        displayManager.lightdm.extraConfig = "logind-check-graphical=true";
+        displayManager.defaultSession = "none+i3";
+        windowManager.i3 = {
+            enable = true;
+            package = pkgs.i3-gaps;
+             extraPackages = with pkgs; [         
+                dmenu   
+                i3lock  
+                i3status
+                i3blocks
+            ];     
+        };   
+    };
+}
