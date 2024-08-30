3a4a2e865444da40d3daeb9d66bb4795c3147449
yakampe <yanis.kampe.cv@gmail.com>
2024-08-30 13:41:10 +0100

    installing nixos script and readme

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
-# TODO
+# Install 
 
-1. need to write the order of things i did when i freshly installed a new one
-2. I had to add enable git
-3. Clone into `~/config`
-3. run nixos install 
-4. reboot
-5. run home manager install
-6. reboot
-
-# TODO2
-
-Create script to move i3config
+1. Add git to `/etc/nixos/configuration.nix` 
+```
+    environment.systemPackages = with pkgs; [
+        pkgs.git
+    ];
+```
+2. `git clone https://github.com/yakampe/nixos-configuration.git` at user home
+3. If username is not `yanis`. Go to `./nix-config/common.nix` and change the `users.users.yanis` and `users.users.yanis.description` to your username
+4. Copy `config-example.json` to `config.json` and fill out the json
+5. Execute `./install-nixos.sh` 
+6. Ensure `install-home-manager.zsh` has correct version according to your NixOS version `https://nix-community.github.io/home-manager/index.xhtml#sec-install-standalone`
+7. Run `./install-home-manager.zsh`
+8. Reboot?
+9. Run `./configuration.sh`
--- install-nixos.sh
+++ install-nixos.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
+# Prompt the user to choose between "laptop" or "desktop"
+echo "Please choose your configuration:"
+echo "1) Laptop"
+echo "2) Desktop"
+read -p "Enter the number for your choice: " choice
+
+# Determine the host type based on user input
+if [ "$choice" == "1" ]; then
+    host_type="laptop"
+elif [ "$choice" == "2" ]; then
+    host_type="desktop"
+else
+    echo "Invalid choice. Please run the script again and choose either 1 or 2."
+    exit 1
+fi
+
 # Pick up your Nix config
 # Remove the existing NixOS configuration file
 sudo rm /etc/nixos/configuration.nix
 
 # Need to copy over the hardware config
-cp /etc/nixos/hardware-configuration.nix $(pwd)/nix-config/hosts/laptop/hardware-configuration.nix
+cp /etc/nixos/hardware-configuration.nix $(pwd)/nix-config/hosts/$host_type/hardware-configuration.nix
 
+# Create a symbolic link to the NixOS configuration file based on the chosen host type
+sudo ln -s $(pwd)/nix-config/hosts/$host_type/configuration.nix /etc/nixos/configuration.nix
 
-# Create a symbolic link to the NixOS configuration file based on the hostname
-sudo ln -s $(pwd)/nix-config/hosts/laptop/configuration.nix /etc/nixos/configuration.nix
-
+# Rebuild NixOS configuration
 sudo nixos-rebuild switch
